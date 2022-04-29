fc4760e901a09d684ed8c808206c496ab38a5464
Gabriel Silva <carlosgabrieldasilvasantana@gmail.com>
2022-04-28 22:15:16 -0300

    Mudando o print

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
   public static void main(String[] args){
-    System.out.println("Ola Mundo");
+    System.out.println("Ola Mundo test");
     System.out.println("Gabriel Passou aqui");
     System.out.println("Endrigo Também :-) ");
     //aaaaaaaaaa
